dd38ad6a330ac2c9230bb848a611fb43d6dbacac
iwate <github@iwate.me>
2021-06-07 21:59:59 +0900

    add challenge2

--- challenge02/dotnet/Program.cs
+++ challenge02/dotnet/Program.cs
@@ -1,32 +1,11 @@
 ﻿using System;
-using System.IO;
-using Google.Protobuf;
-using Iwate.WasmtimeChallenge;
 using Wasmtime;
 
-var bytes = new byte[32];
-using (var stream = new MemoryStream(bytes))
-{
-    new Hello { Message = "Hello, " }.WriteDelimitedTo(stream);
-    stream.Flush();
-}
-
-
 using var engine = new Engine();
-using var module = Module.FromTextFile(engine, "memory.wat");
+using var module = Module.FromFile(engine, "challenge02.wasm");
 using var host = new Host(engine);
-using var mem = host.DefineMemory("", "mem");
-
-for (var i = 0; i < bytes.Length; i++)
-{
-    mem.WriteByte(100 + i, bytes[i]);
-}
 
 using dynamic instance = host.Instantiate(module);
-instance.run();
+var result = instance.add(1, 2);
 
-var result = mem.Span.Slice(100, 32).ToArray();
-using var stream1 = new MemoryStream(result);
-var hello = Hello.Parser.ParseDelimitedFrom(stream1);
-Console.WriteLine(hello.Message);
-Console.WriteLine(Helpers.HexDump(result));
+Console.WriteLine(result);
