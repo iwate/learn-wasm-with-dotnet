2a8c5d29e28988c754c994ca029bbda7e4bb05ef
iwate <github@iwate.me>
2022-02-11 15:28:28 +0900

    reformat

--- challenge05/dotnet/Iwate.WasmtimeChallenge05/Program.cs
+++ challenge05/dotnet/Iwate.WasmtimeChallenge05/Program.cs
@@ -1,46 +1,30 @@
 ﻿using System;
 using System.IO;
-using System.IO.MemoryMappedFiles;
-using System.Text;
 using Wasmtime;
 
-namespace Iwate.Challenge05
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var dir = AppDomain.CurrentDomain.BaseDirectory;
-            var script =
+var dir = AppDomain.CurrentDomain.BaseDirectory;
+var script =
 @"
 import * as host from 'host';
 host.hello();
 ";
-            File.WriteAllText(Path.Combine(dir, "main.js"), script);
+File.WriteAllText(Path.Combine(dir, "main.js"), script);
 
-            using var engine = new Engine();
-            using var module = Module.FromFile(engine, "qjs.wasm");
-            using var linker = new Linker(engine);
-            using var store = new Store(engine);
+using var engine = new Engine();
+using var module = Module.FromFile(engine, "qjs.wasm");
+using var linker = new Linker(engine);
+using var store = new Store(engine);
 
-            linker.DefineFunction("env", "hello", () => { Console.WriteLine("This is called from wasm."); });
+linker.DefineFunction("env", "hello", () => { Console.WriteLine("This is called from wasm."); });
 
-            linker.DefineWasi();
+linker.DefineWasi();
 
-            store.SetWasiConfiguration(new WasiConfiguration()
-                .WithEnvironmentVariable("WASMTIME_BACKTRACE_DETAILS", "1")
-                .WithInheritedStandardInput()
-                .WithInheritedStandardOutput()
-                .WithInheritedStandardError()
-                .WithPreopenedDirectory(dir, ".")
-                .WithArgs("--", "-m", "--std", "main.js"));
+store.SetWasiConfiguration(new WasiConfiguration()
+    .WithPreopenedDirectory(dir, ".")
+    .WithArgs("--", "main.js"));
 
-            var instance = linker.Instantiate(store, module);
+var instance = linker.Instantiate(store, module);
 
-            var run = instance.GetFunction(store, "_start");
+var run = instance.GetFunction(store, "_start");
 
-           
-            run.Invoke(store);
-        }
-    }
-}
+run.Invoke(store);
